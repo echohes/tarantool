2bbe59c3db4b406609321d70560b8267f545d712
Oleg Piskunov <o.piskunov@tarantool.org>
2020-05-18 21:04:48 +0300

    fixing typo

--- .travis.mk
+++ .travis.mk
@@ -120,7 +120,7 @@ test_connector_go_tarantool: build_debian
 	make install
 	wget --progress=dot:mega https://dl.google.com/go/go1.10.linux-amd64.tar.gz
 	tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
-	sudo chmod -R a+rwx /usr/local/go
+	chmod -R a+rwx /usr/local/go
 	export PATH=/usr/local/go/bin:$$PATH && export=/usr/local/go/go-tarantool \
 		&& go get github.com/tarantool/go-tarantool \
 		&& cd /usr/local/go/go-tarantool/src/github.com/tarantool/go-tarantool \
